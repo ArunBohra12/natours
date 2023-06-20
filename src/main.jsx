bfde2299a8c82c33469beba37ee4966b4e8554eb
Arun Bohra <arunbohra12@gmail.com>
2023-06-20 10:57:40 +0530

    Move all frontend inside v1 directory

--- src/main.jsx
+++ src/main.jsx
@@ -1,8 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import App from './App';
 import { BrowserRouter } from 'react-router-dom';
-import { UserProvider } from './context/userContext';
+import App from './v1/App';
+import { UserProvider } from './v1/context/userContext';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <BrowserRouter>
