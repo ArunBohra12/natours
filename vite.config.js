5f5b201d7503e765e34d08e53838b54313c7290b
Arun Bohra <arunbohra12@gmail.com>
2023-07-01 18:37:01 +0530

    Remove pwa config & packages

--- vite.config.js
+++ vite.config.js
@@ -1,34 +1,7 @@
 import { defineConfig } from 'vite';
-import { VitePWA } from 'vite-plugin-pwa';
 import react from '@vitejs/plugin-react';
-
-const PWAManifest = {
-  registerType: 'prompt',
-  includeAssets: ['favicon.png'],
-  manifest: {
-    name: 'Natours',
-    short_name: 'Natours',
-    start_url: '.',
-    display: 'standalone',
-    background_color: '#2b343b',
-    theme_color: '#2e864b',
-    description: 'A tour booking application',
-    icons: [
-      {
-        src: 'favicon.png',
-        sizes: '48x48',
-        type: 'image/png',
-      },
-      {
-        src: 'favicon.png',
-        sizes: '72x72',
-        type: 'image/png',
-      },
-    ],
-  },
-};
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(), VitePWA(PWAManifest)],
+  plugins: [react()],
 });
