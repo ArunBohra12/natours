6ca2d80f06afcbaa9b8868711f40484cf5a30d1b
Arun Bohra <arunbohra12@gmail.com>
2024-02-24 22:58:54 +0530

    Set up localization

--- natours-web/src/App.tsx
+++ natours-web/src/App.tsx
@@ -1,9 +1,13 @@
+import { useTranslation } from 'react-i18next';
+
 import './App.css';
 
 const App = () => {
+  const { t } = useTranslation();
+
   return (
     <div>
-      <div className='text-sm text-green-800'>app</div>
+      <div className='text-sm text-green-800'>{t('key')}</div>
     </div>
   );
 };
--- natours-web/src/main.tsx
+++ natours-web/src/main.tsx
@@ -2,8 +2,12 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App.tsx';
 
+import './localization/i18n';
+
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <App />
+    <React.Suspense fallback='loading...'>
+      <App />
+    </React.Suspense>
   </React.StrictMode>
 );
